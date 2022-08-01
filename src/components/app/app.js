56a0e154d0aee2774d5ff6a02c8f5df52686e121
Andrey <biondohod@gmail.com>
2022-08-01 17:53:24 +0200

    Добавляет динамическое изменение данных о сотруднике
    Пока что только с имитацией получения данных сервера, но скоро подправлю

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,6 +6,12 @@ import EmployeesList from '../employees-list/employees-list';
 import EmployeesAddForm from '../employees-add-form/employees-add-form';
 
 const App = () => {
+    const data = [ 
+        {name: 'Kayn Shieda', salary: 777, increase: false},
+        {name: 'Jesse Mccree', salary: 6969, increase: false},
+        {name: 'Leon Kennedy', salary: 5000, increase: false},
+        {name: 'Jhin Khada', salary: 4444, increase: true},
+    ]
     return (
         <div className="app">
             <AppInfo />
@@ -15,7 +21,7 @@ const App = () => {
                 <AppFilter />
             </div>
 
-            <EmployeesList />
+            <EmployeesList data={data}/>
             <EmployeesAddForm />
         </div>
     );
--- src/components/employees-list/employees-list.js
+++ src/components/employees-list/employees-list.js
@@ -1,12 +1,15 @@
 import './employees-list.css';
 import EmployeesListItem from '../employees-list-item/employees-list-item';
 
-const EmployeesList = () => {
+const EmployeesList = ({data}) => {
+    const items = data.map((item) => {
+        return (
+            <EmployeesListItem {...item}/>
+        );
+    });
     return (
         <ul className="app-list list-group">
-            <EmployeesListItem/>
-            <EmployeesListItem/>
-            <EmployeesListItem/>
+            {items}
         </ul>
     );
 };
